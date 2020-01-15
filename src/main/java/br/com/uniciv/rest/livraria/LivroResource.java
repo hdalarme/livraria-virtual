d7bb876de85377b8dbe34c4c72df117e9e0ccc5c
Helbertt Dalarme <hdalarme@yahoo.com.br>
2020-01-14 22:58:59 -0300

    Aula 01

---
+++ src/main/java/br/com/uniciv/rest/livraria/LivroResource.java
@@ -0,0 +1,23 @@
+package br.com.uniciv.rest.livraria;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("livro")
+public class LivroResource {
+
+	private LivroRepositorio livroRepo = new LivroRepositorio();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Livros getLivros() {
+		Livros livros = new Livros();
+		livros.setLivros(livroRepo.getLivros());
+		return livros;		
+	}
+	
+}
